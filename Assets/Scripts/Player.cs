7f7b7b247a406f74bb3051869110858f81130ebe
aylinsumer <sumeray16@itu.edu.tr>
2021-03-05 00:27:51 +0300

    Player set
    Game ground has finished. Player is set.

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public float speed = 0.1f;
+    public CharacterController myController;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       // myController.Move(Vector3.forward);
+    }
+}
